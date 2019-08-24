57e2baf18ebb51f5258ed058944bf3b78738a537
Bonifase <orwabonifase@gmail.com>
2019-08-24 15:11:19 +0300

    heroku deployment

--- wisdompets/urls.py
+++ wisdompets/urls.py
@@ -1,9 +1,11 @@
 from django.contrib import admin
 from django.conf.urls import url
+from django.conf.urls.static import static
+from django.conf import settings
 from adoptions import views
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^$', views.home, name="home"),
     url(r'^adoptions/(\d+)/', views.pet_detail, name="pet_detail"),
-]
+] + static(settings.STATIC_URL, documen_root=settings.STATICFILES_DIRS)
